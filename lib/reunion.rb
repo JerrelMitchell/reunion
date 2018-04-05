196d3c516d57f73641f772d5c2e53f685b94bc6f
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-05 16:46:42 -0600

    add methods and tests for iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -2,5 +2,14 @@ class Reunion
   attr_reader :location
   def initialize(location)
     @location = location
+    @activities = []
+  end
+
+  def activities
+    @activities.map(&:name)
+  end
+
+  def add_activity(activity)
+    @activities << activity
   end
 end
