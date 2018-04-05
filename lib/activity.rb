d409d7dde585b09396fe46d25c980c4fd883d63d
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-05 15:20:51 -0600

    add methods to determin participants, name of activity, and splitting cost between participants.

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,22 @@
 class Activity
+  attr_reader :name, :participants
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(person, paid)
+    @participants[person] = paid
+  end
+
+  def total_cost
+    @participants.values.sum
+  end
+
+  def split_cost
+    even_stevens = (total_cost / participants.size)
+    @participants.update(participants) do |person, paid|
+      paid = even_stevens
+    end
+  end
 end
